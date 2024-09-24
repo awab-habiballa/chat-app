f649ca609411fb6d398690fec02e7f9e757d0853
awab habiballa <awab.alabady@gmail.com>
2024-09-24 21:35:09 +0400

    feat: Add user model, implement signup API with password encryption and JWT token in cookie

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,44 @@
+import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
+import { errorHandler } from "../util/errorHandler.js";
+import generateTokenAndSetCookie from "../util/generateToken.js";
+
 export const signup = async (req, res, next) => {
   try {
-    const { fullname, username, password, confirmPassword, gender } = req.body;
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+    if (password !== confirmPassword)
+      return next(errorHandler(400, "Passwords do not match"));
+
+    const user = await User.findOne({ username });
+
+    if (user) return next(errorHandler(400, "Username already exists"));
+
+    //hashpassword
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+    const girlProfilePic = `https://api.dicebear.com/9.x/pixel-art/svg?seed=${username}`;
+    const boyProfilePic = `https://api.dicebear.com/9.x/pixel-art/svg?seed=${username}`;
+
+    const newUser = new User({
+      fullName,
+      username,
+      password: hashedPassword,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      next(errorHandler(400, "Invalid user data"));
+    }
   } catch (error) {
     next(error);
   }
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-dotenv.config();
-app.use(express.json());
-
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Server is ready !!");
-});
+dotenv.config();
+app.use(express.json()); // to parse the incoming request with json payload
 
 app.use("/api/auth", authRoutes);
 
