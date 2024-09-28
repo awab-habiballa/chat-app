57b22274effef00229936d922e20c9f318328f0b
awab habiballa <awab.alabady@gmail.com>
2024-09-28 16:40:50 +0400

    complete get messages api routes

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Messsage from "../models/message.model.js";
+import { errorHandler } from "../util/errorHandler.js";
 
 export const sendMessage = async (req, res, next) => {
   try {
@@ -26,12 +27,32 @@ export const sendMessage = async (req, res, next) => {
     if (newMessage) {
       conversation.messages.push(newMessage._id);
     }
+
+    //Socket IO FUNCTIONALITY
     //await conversation.save();
     //await newMessage.save();
 
     await Promise.all([conversation.save(), newMessage.save()]); //to run at the same time
 
     res.status(201).json(newMessage);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getMessages = async (req, res, next) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages"); // to get the actual
+
+    if (!conversation) return res.status(200).json([]);
+    const messages = conversation.messages;
+
+    res.status(200).json(messages);
   } catch (error) {
     next(error);
   }
